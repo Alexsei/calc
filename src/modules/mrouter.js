a984a91780c16d12126acb65273545a0dec03101
Alexsei <VopilovskiiAleksei@gmail.com>
2019-09-14 19:16:16 +0700

    up

--- src/modules/mrouter.js
+++ src/modules/mrouter.js
@@ -1,13 +1,20 @@
-const mrouter = {
-  data
-};
-class router {
-  constructor {
+const mrouter = new Router();
+
+class Router {
+  constructor() {
     this.data = [];
 
   }
-  setAdd = function (name, url, path) {
+  setAdd (name, url, path) {
     this.data.push({name: name, url:url, path: path })
+  }
+  getURL (url) {
+      let res = false;
+      let data = this.data;
+    for (let i = 0; i < data.lenght; i++) {
+      if (url == data[i].url) res = data[i]
+    }
+    return res;
   }
 };
 
